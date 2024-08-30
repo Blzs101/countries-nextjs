ade79206411daa0e3558fb8d5ecb25888b43cb9b
Kinczel Balázs <kinc.kb@gmail.com>
2024-08-30 21:41:03 +0200

    removed tanstack query

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,12 +1,9 @@
 "use client"
 import { ThemeProvider } from 'next-themes'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 export default function ThemeContext({ children }: { children: React.ReactNode }) {
     return (
-        <QueryClientProvider client={new QueryClient()}>
-            <ThemeProvider themes={["dark", "light"]} enableSystem={false} attribute="class" defaultTheme="dark">
-                {children}
-            </ThemeProvider>
-        </QueryClientProvider>
+        <ThemeProvider themes={["dark", "light"]} enableSystem={false} attribute="class" defaultTheme="dark">
+            {children}
+        </ThemeProvider>
     )
 }
