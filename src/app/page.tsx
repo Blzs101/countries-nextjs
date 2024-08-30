048ad835b90d064ee9b6ad32a16f9b2257789015
Kinczel Balázs <kinc.kb@gmail.com>
2024-08-30 16:21:50 +0200

    added type safty to fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {data &&
-        data.map((country: any) => (
+        data.map((country) => (
           <h1 key={country.name.common}>{country.name.common}</h1>
         ))
       }
--- src/util/countriesFetch.ts
+++ src/util/countriesFetch.ts
@@ -2,7 +2,29 @@ import { cache } from "react";
 
 export const fetchData = cache(async () => {
 	const response = await fetch("https://restcountries.com/v3.1/all");
-	const data = await response.json();
+	const data: elementType[] = await response.json();
 
 	return data;
 });
+
+type elementType = {
+	flags: {
+		svg: string;
+		alt: string;
+	};
+	name: {
+		common: string;
+		nativeName: object;
+	};
+	population: number;
+	region: string;
+	capital: string[];
+	subRegion: string;
+	tld: string[];
+	languages: string;
+	currencies: {
+		name: string;
+		symbol: string;
+	}[];
+	borders: string[];
+};
