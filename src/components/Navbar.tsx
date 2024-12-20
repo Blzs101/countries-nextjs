af4c019084c9be89231460eca735264a9384ffa2
Kinczel Balázs <kinc.kb@gmail.com>
2024-12-20 20:04:28 +0100

    changed <a> to <Link>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
 import ModeToggle from "./ModeToggle";
-
 export default function Navbar() {
+
   return (
     <header className="mb-4 flex h-[72px] items-center justify-center bg-very-dark-grey-bg text-black dark:bg-dark-blue-element dark:text-white">
-      <a href="/" className="mr-auto p-6 font-extrabold">
+      <Link href="/" className="mr-auto p-6 font-extrabold">
         <h1>Where in the word?</h1>
-      </a>
+      </Link>
       <div className="flex p-6">
         <ModeToggle />
       </div>
