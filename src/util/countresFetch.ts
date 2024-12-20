00e12a4d7385094f7330dff1c0d6cf7a438b620f
Kinczel Balázs <kinc.kb@gmail.com>
2024-12-20 20:14:41 +0100

    changed fetch cache

--- src/util/countresFetch.ts
+++ src/util/countresFetch.ts
@@ -2,7 +2,9 @@ import { elementType } from "./countresType";
 
 export async function fetchCountries() {
     const res = await fetch("https://restcountries.com/v3.1/all",{
-        cache: "force-cache"
+        next: {
+            revalidate: 60 * 60 * 24,
+        },
     });
     const data = await res.json();
     return data as elementType[]; 
